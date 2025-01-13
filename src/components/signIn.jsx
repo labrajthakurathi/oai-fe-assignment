fe9119e96786e91fb3be925358963519da1b0ee7
lab raj thakurathi <44740227+labrajthakurathi@users.noreply.github.com>
2025-01-13 10:47:46 -0800

    fixed heroku log

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -7,86 +7,98 @@ import Footer from "./footer";
 import Header from "./header";
 
 const SignIn = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    navigate("/library");
-  };
-  return (
-    <>
-      <Header />
-      <div className="page">
-        <div className="sign-in">
-          <div className="intro">
-            <div className="person-wrapper">
-              <Person />
-            </div>
-            <p className="sm-semibold">Sign In</p>
-            <p className="md-regular welcome">
-              Welcome back! Let’s get started with AI
-            </p>
-          </div>
-          <div className="form">
-            <form onSubmit={handleSubmit}>
-              <div className="item">
-                <label htmlFor="email" className="sm-medium label-color">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  placeholder="Enter your email"
-                  required
-                />
-              </div>
-              <div className="item">
-                <label htmlFor="password" className="sm-medium label-color">
-                  Password
-                </label>
-                <input
-                  type="text"
-                  name="password"
-                  id="password"
-                  placeholder="Create a password"
-                  required
-                />
-              </div>
-              <div className="item">
-                <button className="primary-btn" type="submit">
-                  Sign In
-                </button>
-              </div>
-            </form>
-            <div className="or">
-              <div></div>
-              <p>OR</p>
-              <div></div>
-            </div>
-            <div className="third-party-auth">
-              <button className="secondary-btn">
-                <Google /> Sign up with Google
-              </button>
-              <button className="secondary-btn">
-                <LinkedIn />
-                Sign up with Linkedin
-              </button>
-            </div>
-          </div>
+		navigate("/library");
+	};
+	return (
+		<>
+			<Header />
+			<div className='page'>
+				<div className='sign-in'>
+					<div className='intro'>
+						<div className='person-wrapper'>
+							<Person />
+						</div>
+						<p className='sm-semibold'>Sign In</p>
+						<p className='md-regular welcome'>
+							Welcome back! Let’s get started with AI
+						</p>
+					</div>
+					<div className='form'>
+						<form onSubmit={handleSubmit}>
+							<div className='item'>
+								<label
+									htmlFor='email'
+									className='sm-medium label-color'
+								>
+									Email
+								</label>
+								<input
+									type='text'
+									name='email'
+									id='email'
+									placeholder='Enter your email'
+									required
+								/>
+							</div>
+							<div className='item'>
+								<label
+									htmlFor='password'
+									className='sm-medium label-color'
+								>
+									Password
+								</label>
+								<input
+									type='text'
+									name='password'
+									id='password'
+									placeholder='Create a password'
+									required
+								/>
+							</div>
+							<div className='item'>
+								<button
+									className='primary-btn'
+									type='submit'
+								>
+									Sign In
+								</button>
+							</div>
+						</form>
+						<div className='or'>
+							<div></div>
+							<p>OR</p>
+							<div></div>
+						</div>
+						<div className='third-party-auth'>
+							<button className='secondary-btn'>
+								<Google /> Sign up with Google
+							</button>
+							<button className='secondary-btn'>
+								<LinkedIn />
+								Sign up with Linkedin
+							</button>
+						</div>
+					</div>
 
-          <div className="sign-up-link">
-            <p className="sm-regular">Don’t have an account?</p>{" "}
-            <a href="#" className="sm-semibold">
-              Sign up
-            </a>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+					<div className='sign-up-link'>
+						<p className='sm-regular'>Don’t have an account?</p>{" "}
+						<a
+							href='#'
+							className='sm-semibold'
+						>
+							Sign up
+						</a>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default SignIn;
